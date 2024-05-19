7b605daa8ca3b38c42879ac7ac1c802ed8d8419f
merodeadorNocturno <merodeador.nocturno@gmail.com>
2024-05-19 17:22:50 -0600

    add env vars to front.

---
+++ env.sh
@@ -0,0 +1,20 @@
+# env.sh
+# echo "ev = {"
+# echo "  protocol: 'https'"
+# echo "  address: 'crm.yayleads.mx'"
+# echo "  port: '80'"
+# echo "  be_port: '443'"
+# echo "  be_protocol: 'https'"
+# echo "  be_address: 'api.yayleads.mx'"
+# echo "}"
+
+echo "ev = {"
+echo "  protocol: 'http',"
+echo "  address: '0.0.0.0',"
+echo "  port: '8081',"
+echo "  be_port: '8080',"
+echo "  be_protocol: 'http',"
+echo "  be_address: '0.0.0.0',"
+echo "}"
+
+# ./env.sh.example > ./static/env.js
